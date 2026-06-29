35397269250c2799af760059d1d6d1b0ef3c5f90
Roger Calvas <sebascalvas@outlook.com>
2026-06-28 21:53:06 -0500

    jupyter

--- nvim/lua/plugins/jupyter.lua
+++ nvim/lua/plugins/jupyter.lua
@@ -1,21 +1,28 @@
 return {
   {
     "benlubas/molten-nvim",
+
     build = ":UpdateRemotePlugins",
     init = function()
-      -- Opciones de configuración
+      -- Configuraciones visuales
       vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
+      vim.g.molten_wrap_output = true
     end,
-  },
-  -- Dependencia para mostrar imágenes en la terminal (requiere ueberzugpp, kitty o wezterm)
-  {
-    "3rd/image.nvim",
-    opts = {
-      backend = "kitty", -- Cambia esto según tu terminal (wezterm, ueberzug)
-      integrations = {
-        markdown = { enabled = true },
+    -- Aquí definimos los atajos de teclado mágicos (todos empiezan con <leader>m)
+    keys = {
+      { "<leader>mi", ":MoltenInit<CR>", desc = "Molten: Inicializar Kernel", silent = true },
+      { "<leader>ml", ":MoltenEvaluateLine<CR>", desc = "Molten: Evaluar Línea", silent = true },
+      {
+        "<leader>me",
+        ":<C-u>MoltenEvaluateVisual<CR>gv",
+        mode = "v",
+        desc = "Molten: Evaluar Selección",
+        silent = true,
       },
+      { "<leader>md", ":MoltenDelete<CR>", desc = "Molten: Borrar Output", silent = true },
+      { "<leader>mo", ":MoltenShowOutput<CR>", desc = "Molten: Mostrar Output en Ventana", silent = true },
     },
   },
+  -- ... (aquí sigue tu configuración de image.nvim si la pusiste)
 }
