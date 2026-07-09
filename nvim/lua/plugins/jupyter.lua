e8a51a6565bc2076979005b7bf54c680d0e6932f
Roger Calvas <sebascalvas@outlook.com>
2026-07-09 12:22:31 -0400

    changes in nvim and mega base to better run molten

--- nvim/lua/plugins/jupyter.lua
+++ nvim/lua/plugins/jupyter.lua
@@ -8,6 +8,32 @@ return {
       vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
       vim.g.molten_wrap_output = true
+
+      -- Evaluador personalizado para celdas de Python
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "python" },
+        callback = function()
+          vim.keymap.set("n", "<leader>mc", function()
+            local r = vim.api.nvim_win_get_cursor(0)[1]
+            local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+            local start_line = 1
+            for i = r, 1, -1 do
+              if lines[i]:match("^# %%%s*") or lines[i]:match("^# %%$") then
+                start_line = i
+                break
+              end
+            end
+            local end_line = #lines
+            for i = r + 1, #lines do
+              if lines[i]:match("^# %%%s*") or lines[i]:match("^# %%$") then
+                end_line = i - 1
+                break
+              end
+            end
+            vim.fn.MoltenEvaluateRange(start_line, end_line)
+          end, { desc = "Molten: Evaluar Celda (Python)", buffer = true, silent = true })
+        end,
+      })
     end,
     -- Aquí definimos los atajos de teclado mágicos (todos empiezan con <leader>m)
     keys = {
@@ -21,8 +47,9 @@ return {
         silent = true,
       },
       { "<leader>md", ":MoltenDelete<CR>", desc = "Molten: Borrar Output", silent = true },
-      { "<leader>mo", ":MoltenShowOutput<CR>", desc = "Molten: Mostrar Output en Ventana", silent = true },
-      { "<leader>mc", ":MoltenReevaluateCell<CR>", desc = "Molten: Evaluar Celda (# %%)", silent = true },
+      { "<leader>mo", ":MoltenExportOutput<CR>", desc = "Molten: Mostrar Output en split", silent = true },
+      { "<leader>mO", ":MoltenShowOutput<CR>", desc = "Molten: Volver a mostrar Output en Ventana", silent = true },
+      { "<leader>mC", ":MoltenReevaluateCell<CR>", desc = "Molten: Reevaluar Celda", silent = true },
     },
   },
   --   -- Dependencia para mostrar imágenes en la terminal (requiere ueberzugpp, kitty o wezterm)
